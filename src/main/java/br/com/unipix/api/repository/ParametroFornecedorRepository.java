35ae318627142dcf1697828c42a9b7d66fef659c
Fernando <fstanchese@gmail.com>
2021-10-03 19:21:46 -0300

    ajustes

--- src/main/java/br/com/unipix/api/repository/ParametroFornecedorRepository.java
+++ src/main/java/br/com/unipix/api/repository/ParametroFornecedorRepository.java
@@ -9,6 +9,6 @@ import br.com.unipix.api.model.ParametroFornecedor;
 
 public interface ParametroFornecedorRepository extends JpaRepository<ParametroFornecedor, Integer> {
 
-	@Query("select f from ParametroFornecedorSMS f where f.fornecedorSMSID = ?1")
+	@Query("select f from ParametroFornecedor f where f.fornecedorSMSID = ?1")
 	public List<ParametroFornecedor> findByfornecedorSMSID(Integer fornecedorSMSID);
 }
